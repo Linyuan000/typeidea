4b2469701c177502e37f8ca649f978c25c99aa3e
Lingyuan <256084268@qq.com>
2020-03-09 00:18:32 +0800

    增加url

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from blog.views import IndexView, CategoryView, TagView, PostDetailView
+from blog.views import IndexView, CategoryView, TagView, PostDetailView, SearchView, AuthorView
+from comment.views import CommentView
 # from blog.views import post_list, post_detail
-from config.views import links
+from config.views import LinkListView
 from .custom_site import custom_site
 
 
@@ -32,8 +33,10 @@ urlpatterns = [
     url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'), #class-based view 的url
     url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
     url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
-
-    url(r'^links/$', links, name='links'),
+    url(r'^search/$', SearchView.as_view(), name='search'),
+    url(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author'),
+    url(r'^links/$',LinkListView.as_view(),  name='links'),
+    url(r'^comment/$', CommentView.as_view(), name='comment'),
     url(r'^super_admin/', admin.site.urls, name='super-admin'),
     url(r'^admin/', custom_site.urls, name='admin'),
 ]
